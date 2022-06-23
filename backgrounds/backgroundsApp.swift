89225cc156f98b9ae11aa647e76bde0ca8ae7b01
ab-80 <63623441+ab-80@users.noreply.github.com>
2022-06-23 18:22:57 -0500

    Initial Commit

---
+++ backgrounds/backgroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  backgroundsApp.swift
+//  backgrounds
+//
+//  Created by Andrew Bergerson on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct backgroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
